174bf967bce692e82c716835c5f737518a3319ee
Brandon Fernandez Mejia <=>
2020-11-08 22:16:23 -0400

    new to

--- SegundoParcialProg2/app/src/main/java/com/example/segundoparcialprog2/MainActivity.java
+++ SegundoParcialProg2/app/src/main/java/com/example/segundoparcialprog2/MainActivity.java
@@ -3,16 +3,20 @@ package com.example.segundoparcialprog2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.CheckBox;
+import android.text.Html;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView titulo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
-        CheckBox check = findViewById(R.id.);
+        titulo =  findViewById(R.id.tituloprincipal);
+        titulo.setText(Html.fromHtml("<b>REAL QUIZ PROGRAMACION 2<BR/>BRANDOX MEJIA</b>"));
 
         android.widget.Toast.makeText(this, "1a es: ", Toast.LENGTH_SHORT).show();
 
