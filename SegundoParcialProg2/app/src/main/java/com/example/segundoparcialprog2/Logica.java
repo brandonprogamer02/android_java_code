acc3912e2ba6dda2fd6c27ccc6f14dd0e84fba48
Brandon Fernandez Mejia <=>
2020-11-10 00:25:07 -0400

    hola people que esta leyendo esto

--- SegundoParcialProg2/app/src/main/java/com/example/segundoparcialprog2/Logica.java
+++ SegundoParcialProg2/app/src/main/java/com/example/segundoparcialprog2/Logica.java
@@ -1,24 +1,34 @@
 package com.example.segundoparcialprog2;
 
 import android.content.res.ColorStateList;
+import android.view.View;
 import android.widget.RadioGroup;
 
 public class Logica {
+    private static Logica instance;
+    private Logica() {}
 
-    public static int getPuntuacion(){
+    public static Logica getInstance(){
+        if(instance == null) instance = new Logica();
+        return instance;
+    }
+    public int getPuntuacion(){
         int puntuacion = 0;
         if (MainActivity.unoa.isChecked()) puntuacion +=5;
         if(MainActivity.dosb.isChecked()) puntuacion +=5;
         if(MainActivity.tresa.isChecked()) puntuacion +=5;
         if(MainActivity.cuatroa.isChecked()) puntuacion +=5;
         if(MainActivity.cincoa.isChecked()) puntuacion +=5;
-
         return puntuacion;
     }
-    public static void resolver(){
+    public void resolver(){
         int puntuacion = getPuntuacion();
-        MainActivity.resultado.setText(String.format("Tu puntuacion fue de: %s",puntuacion));
-
+        MainActivity.resultado.setText(String.format("Tu puntuacion fue de: %s/20",puntuacion));
+        showAnswers();
+    }
+    private void showAnswers(){
+        MainActivity.respuesta1.setVisibility(View.VISIBLE); MainActivity.respuesta2.setVisibility(View.VISIBLE); MainActivity.respuesta3.setVisibility(View.VISIBLE);
+        MainActivity.respuesta4.setVisibility(View.VISIBLE); MainActivity.respuesta5.setVisibility(View.VISIBLE);
     }
 
 }
--- SegundoParcialProg2/app/src/main/java/com/example/segundoparcialprog2/MainActivity.java
+++ SegundoParcialProg2/app/src/main/java/com/example/segundoparcialprog2/MainActivity.java
@@ -12,30 +12,42 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    static TextView titulo,resultado;
+    static TextView titulo,resultado,respuesta1,respuesta2,respuesta3,respuesta4,respuesta5;
     static RadioGroup grupo1;
     static RadioButton unoa,unob,unoc,unod,dosa,dosb,dosc,dosd,tresa,tresb,tresc,tresd,cuatroa,cuatrob,cuatroc,cuatrod,cincoa,cincob,cincoc,cincod;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // cargamos las importanciones del xml
+        //----------------------------------------------------------------------------------------------------------------
         cargarImportacionaesXML();
-        //-----------------------------------------------------------------------------------------------------------------
+        loadEvents();
 
+        //-----------------------------------------------------------------------------------------------------------------
         titulo.setText(Html.fromHtml("<b>REAL QUIZ PROGRAMACION 2<BR/>BRANDOX MEJIA</b>"));
 
+    }
+    public void loadEvents(){
         findViewById(R.id.verResultado).setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                Logica.resolver();
+                Logica.getInstance().resolver();
             }
         });
+        findViewById(R.id.salir).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                System.exit(0);
+            }
+        });
+
 
     }
     public void cargarImportacionaesXML(){
         titulo =  findViewById(R.id.tituloprincipal);
         resultado =  findViewById(R.id.resultado);
+        respuesta1  = findViewById(R.id.respuesta1); respuesta2  = findViewById(R.id.respuesta2); respuesta3  = findViewById(R.id.respuesta3);
+        respuesta4  = findViewById(R.id.respuesta4);respuesta5  = findViewById(R.id.respuesta5);
         grupo1 = findViewById(R.id.radioGroup1);
         unoa = findViewById(R.id.unoa); unob = findViewById(R.id.unob); unoc = findViewById(R.id.unoc); unod = findViewById(R.id.unod);
         dosa = findViewById(R.id.dosa); dosb = findViewById(R.id.dosb); dosc = findViewById(R.id.dosc); dosd = findViewById(R.id.dosd);
@@ -43,5 +55,6 @@ public class MainActivity extends AppCompatActivity {
         cuatroa = findViewById(R.id.cuatroa); cuatrob = findViewById(R.id.cuatrob); cuatroc = findViewById(R.id.cuatroc); cuatrod = findViewById(R.id.cuatrod);
         cincoa = findViewById(R.id.cincoa); cincob = findViewById(R.id.cincob); cincoc = findViewById(R.id.cincoc); cincod = findViewById(R.id.cincod);
     }
+
 
 }
